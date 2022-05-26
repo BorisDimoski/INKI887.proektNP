85070a77da7a687beb9a0f0b324b7f7979ae52b0
BorisDimoski <borisdimoski03@gmail.com>
2022-05-26 14:20:23 +0200

    vnes na komentari za objasnuvanje na kodot

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <fstream>
 using namespace std;
+//funkcija za najgolema cifra
 int najg(int a)
 {
     int x=-999;
@@ -16,6 +17,7 @@ int najg(int a)
 }
 int main()
 {
+//deklaracija na promenlivi
     string str="INKI887Boris Huey Lewis and the News I know that it's crazy I know that it's nowhere But there is no denying that it's hip to be square";
     int br,n,i;
     vector<int> v;
@@ -31,7 +33,7 @@ int main()
     cout<<"----------------------------------------------------------------------------------------------------------------------"<<endl;
     cout<<str;
     cout<<endl;
-    for(i=0;i<str.size();i++)
+    for(i=0;i<str.size();i++)       //promena na ASCII vrednost i vnesuvanje vo vektorot
     {
         str[i]+=n;
         v.push_back(str[i]);
@@ -39,13 +41,13 @@ int main()
     cout<<"----------------------------------------------------------------------------------------------------------------------"<<endl;
     cout<<"Stringot so smeneti ASCII karakteri:"<<endl;
     cout<<str;
-    sort(v.begin(),v.end());
+    sort(v.begin(),v.end());        //algoritam za sortiranje na vektorot
     cout<<endl;
     cout<<"----------------------------------------------------------------------------------------------------------------------"<<endl;
     cout<<"Sortiran vektor:"<<endl;
     cout<<endl;
-    dat.open("INKI887.proekt3.txt", ios::out | std::ofstream::trunc);
-    for(i=0;i<v.size();i++)
+    dat.open("INKI887.proekt3.txt", ios::out | std::ofstream::trunc); //otvaranje na datoteka INKI887.proekt3.txt, brisenje na prethodna sodrzina
+    for(i=0;i<v.size();i++)                                           //i vnesuvanje na nova
     {
         cout<<(char)v[i];
         dat<<(char)v[i];
